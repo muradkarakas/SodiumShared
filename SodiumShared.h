4e72b72fdfb5782af36253de8a82588ff9e933e1
Murad Karakas <muradkarakas@gmail.com>
2020-06-04 01:12:23 +0300

    Some modifications has been made for SodiumCompiler

--- SodiumShared.h
+++ SodiumShared.h
@@ -11,10 +11,14 @@
  *	https://choosealicense.com/licenses/gpl-3.0/
  */
 
-#ifndef  _HTSQL_SHARED_H
-#define _HTSQL_SHARED_H
+#pragma once
 
-#define SODIUM_SHARED_API				__declspec(dllexport)
+#if  defined(_SODIUM_SHARED)
+	#define SODIUM_SHARED_API				__declspec(dllexport)
+#else
+	#define SODIUM_SHARED_API				__declspec(dllimport)
+#endif
+
 
 #include "pch.h"
 
@@ -30,21 +34,21 @@
 
 typedef void * HANDLE;
 
-	SODIUM_SHARED_API void		mkCoreDebug(const char *file, int line, const char *first, ...);
-	SODIUM_SHARED_API char		* mkItoa(size_t value, char* result);
-	SODIUM_SHARED_API void		mkFree(HANDLE pHeapHandle, void *ptr);
-	SODIUM_SHARED_API void		* mkMalloc(HANDLE pHeapHandle, size_t memsize, const char *sourceFile, int sourceLine);
-	SODIUM_SHARED_API void		mkFreeAdv(HANDLE pHeapHandle, void **ptr);
-	SODIUM_SHARED_API void		* mkReAlloc(HANDLE pHeapHandle, void *ptr, size_t newmemsize);
-	SODIUM_SHARED_API char		* mkStrcat(HANDLE pHeapHandle, const char *sourceFile, int sourceLine, const char * first, ...);
-	SODIUM_SHARED_API wchar_t   * mkStrcatW(HANDLE pHeapHandle, const char* sourceFile, int sourceLine, const wchar_t* first, ...);
-	SODIUM_SHARED_API char		* mkStrdup(HANDLE pHeapHandle, const char *sourceStr, const char *sourceFile, int sourceLine);
-	SODIUM_SHARED_API wchar_t	* mkStrdupW(HANDLE pHeapHandle, const wchar_t *sourceStr, const char *sourceFile, int sourceLine);
-	SODIUM_SHARED_API char		* mkStrdupN(HANDLE pHeapHandle, const char *sourceStr, int len, const char *sourceFile, int sourceLine);
-	SODIUM_SHARED_API int		mkIsStringEmpty(const char *str);
-	SODIUM_SHARED_API void		mkLowerCaseDriveLetterW(wchar_t* s);
-	SODIUM_SHARED_API void		mkLowerCaseDriveLetterA(char* s);
-	SODIUM_SHARED_API wchar_t*		GetFileName(wchar_t* path);
 
-#endif // ! _HTSQL_SHARED_H
+SODIUM_SHARED_API void		mkCoreDebug(const char *file, int line, const char *first, ...);
+SODIUM_SHARED_API char		* mkItoa(size_t value, char* result);
+SODIUM_SHARED_API void		mkFree(HANDLE pHeapHandle, void *ptr);
+SODIUM_SHARED_API void		* mkMalloc(HANDLE pHeapHandle, size_t memsize, const char *sourceFile, int sourceLine);
+SODIUM_SHARED_API void		mkFreeAdv(HANDLE pHeapHandle, void **ptr);
+SODIUM_SHARED_API void		* mkReAlloc(HANDLE pHeapHandle, void *ptr, size_t newmemsize);
+SODIUM_SHARED_API char		* mkStrcat(HANDLE pHeapHandle, const char *sourceFile, int sourceLine, const char * first, ...);
+SODIUM_SHARED_API wchar_t   * mkStrcatW(HANDLE pHeapHandle, const char* sourceFile, int sourceLine, const wchar_t* first, ...);
+SODIUM_SHARED_API char		* mkStrdup(HANDLE pHeapHandle, const char *sourceStr, const char *sourceFile, int sourceLine);
+SODIUM_SHARED_API wchar_t	* mkStrdupW(HANDLE pHeapHandle, const wchar_t *sourceStr, const char *sourceFile, int sourceLine);
+SODIUM_SHARED_API char		* mkStrdupN(HANDLE pHeapHandle, const char *sourceStr, int len, const char *sourceFile, int sourceLine);
+SODIUM_SHARED_API int		mkIsStringEmpty(const char *str);
+SODIUM_SHARED_API void		mkLowerCaseDriveLetterW(wchar_t* s);
+SODIUM_SHARED_API void		mkLowerCaseDriveLetterA(char* s);
+SODIUM_SHARED_API wchar_t*		GetFileName(wchar_t* path);
+
 
